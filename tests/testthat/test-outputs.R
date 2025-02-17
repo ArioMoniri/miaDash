d9a01503742c1abe73f29521e2a5e131b3821c11
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2025-02-17 13:10:54 +0300

    Update test-outputs.R

--- tests/testthat/test-outputs.R
+++ tests/testthat/test-outputs.R
@@ -1,5 +1,4 @@
 test_that("outputs", {
-
     output <- new.env()
     rObjects <- new.env()
   
@@ -9,5 +8,27 @@ test_that("outputs", {
     expect_null(overview_out)
     expect_null(download_out)
     expect_named(output, c("object", "download"))
+})
+
+test_that("outputs handle alternative experiments", {
+    output <- new.env()
+    rObjects <- new.env()
     
+    # Create a mock TreeSummarizedExperiment with an alternative experiment
+    data("Tengeler2020", package = "mia")
+    tse <- Tengeler2020
+    alt_exp <- SummarizedExperiment(
+        assays = list(counts = matrix(1:12, nrow = 3, ncol = 4))
+    )
+    altExp(tse, "test_alt") <- alt_exp
+    rObjects$tse <- tse
+    
+    # Test that overview shows alternative experiments
+    overview_out <- .render_overview(output, rObjects)
+    expect_null(overview_out)
+    
+    # Test that download includes alternative experiments
+    download_out <- .render_download(output, rObjects)
+    expect_null(download_out)
+    expect_named(output, c("object", "download"))
 })
